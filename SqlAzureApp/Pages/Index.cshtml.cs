c3f496cced15ecbfbb4f90be415581fb2beb74e9
imanasaad <imanasaad@gmail.com>
2023-03-22 18:58:13 +0200

    First commit

---
+++ SqlAzureApp/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SqlAzureApp.Models;
+using SqlAzureApp.Services;
+
+namespace SqlAzureApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //private readonly ILogger<IndexModel> _logger;
+
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+        public List<Product> prodList = new List<Product>();
+
+        public void OnGet()
+        {
+            ProductService prodService = new ProductService();
+            prodList=prodService.GetProducts();
+        }
+    }
+}
